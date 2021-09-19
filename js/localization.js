dd3034885d17f07af37cbacd5f2cb3909e67cc11
BlackKingWhitePawn <riabtsev.nikita@ya.ru>
2021-09-19 16:43:17 +0500

    t4: check for exceptions

--- js/localization.js
+++ js/localization.js
@@ -9,6 +9,7 @@ const languages = {
         company: 'company',
         city: 'city',
         message: 'message',
+        send: 'send',
     },
     ru: {
         title: 'Название формы',
@@ -18,6 +19,7 @@ const languages = {
         company: 'Компания',
         city: 'Город',
         message: 'Текст сообщения',
+        send: 'Отправить',
     },
     ar: {
         title: 'لقب شكل',
@@ -27,6 +29,7 @@ const languages = {
         company: 'شركة',
         city: 'مدينة',
         message: 'رسالة',
+        send: 'يرسل',
     },
 }
 
@@ -43,21 +46,37 @@ Array.from(container.children).forEach((element) =>
  */
 function switchLanguage(lang) {
     Array.from(document.getElementsByTagName('p'))
-        .concat(Array.from(document.getElementById('title')))
+        .concat([document.getElementById('title'), document.getElementById('send')])
         .forEach((element) => _translateElementValue(element, languages, lang))
 }
 
 /**
  * Replace heading/paragraph element`s value to its translation
- * @param {HTMLElement} element - HTMLParagraphElement or HTMLHeadingElement
+ * @param {HTMLElement} element - HTMLParagraphElement || HTMLHeadingElement || HTMLInputElement
  * @param {Object} languages - localization object
  * @param {String} lang - language to change in ISO 639-1 format
  */
 function _translateElementValue(element, languages, lang) {
+    _checkElementLocalization(element, languages, lang)
+    if (element.nodeName === 'INPUT') element.value = languages[lang][element.id]
+    else element.firstChild.nodeValue = languages[lang][element.id]
+}
+
+/**
+ * Check that element has id, language is in dictionary, element could be translated and has correct type
+ * @param {HTMLElement} element - element to check
+ * @param {Object} languages - localization object
+ * @param {String} lang - language to change in ISO 639-1 format
+ */
+function _checkElementLocalization(element, languages, lang) {
+    if (!lang) throw 'Incorrect language'
+    if (lang.length != 2) throw TypeError('Incorrect language format, should be in ISO 639-1')
     if (!element.id) throw 'Element has no id'
     if (languages[lang] === null) throw `There is no ${lang} in localization`
     if (languages[lang][element.id] === null) throw `There is no translation for ${element.id}`
-    if (['P', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'].indexOf(element.nodeName) < 0)
+    if (
+        element.nodeName !== 'INPUT' &&
+        ['P', 'H1', 'H2', 'H3', 'H4', 'H5', 'H6'].indexOf(element.nodeName) < 0
+    )
         throw TypeError('Missing element type')
-    element.firstChild.nodeValue = languages[lang][element.id]
 }
