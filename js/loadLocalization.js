3a3009ebe523871fabf3b4300ea090da4710e80d
BlackKingWhitePawn <riabtsev.nikita@ya.ru>
2021-09-19 17:24:25 +0500

    t4: change localization

---
+++ js/loadLocalization.js
@@ -0,0 +1,32 @@
+const languages = {
+    en: {
+        title: 'Form title',
+        first_name: 'first name',
+        last_name: 'last name',
+        email: 'email',
+        company: 'company',
+        city: 'city',
+        message: 'message',
+        send: 'send',
+    },
+    ru: {
+        title: 'Название формы',
+        first_name: 'Имя',
+        last_name: 'Фамилия',
+        email: 'E-mail',
+        company: 'Компания',
+        city: 'Город',
+        message: 'Текст сообщения',
+        send: 'Отправить',
+    },
+    ar: {
+        title: 'لقب شكل',
+        first_name: 'اسم',
+        last_name: 'لقب',
+        email: 'البريد الإلكتروني',
+        company: 'شركة',
+        city: 'مدينة',
+        message: 'رسالة',
+        send: 'يرسل',
+    },
+}
--- js/localization.js
+++ js/localization.js
@@ -1,38 +1,4 @@
 // import languages from '../assets/localization.json'
-
-const languages = {
-    en: {
-        title: 'Form title',
-        first_name: 'first name',
-        last_name: 'last name',
-        email: 'email',
-        company: 'company',
-        city: 'city',
-        message: 'message',
-        send: 'send',
-    },
-    ru: {
-        title: 'Название формы',
-        first_name: 'Имя',
-        last_name: 'Фамилия',
-        email: 'E-mail',
-        company: 'Компания',
-        city: 'Город',
-        message: 'Текст сообщения',
-        send: 'Отправить',
-    },
-    ar: {
-        title: 'لقب شكل',
-        first_name: 'اسم',
-        last_name: 'لقب',
-        email: 'البريد الإلكتروني',
-        company: 'شركة',
-        city: 'مدينة',
-        message: 'رسالة',
-        send: 'يرسل',
-    },
-}
-
 const container = document.getElementById('switcherContainer')
 Array.from(container.children).forEach((element) =>
     element.addEventListener('change', () => {
